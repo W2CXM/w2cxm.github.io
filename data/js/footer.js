bf76ed34d845b68f2187cd8b92ff62fb72054974
Avram Dreyer <47763732+Aviboi@users.noreply.github.com>
2026-09-07 19:38:30 -0400

    Update copyright footer and add no-opener no-referrer to links

--- data/js/footer.js
+++ data/js/footer.js
@@ -6,11 +6,11 @@ var footer_data = `<div style="padding: 2vh;"></div>
         <div></div>
         <div style="text-align: left; padding: 1rem 10%; overflow-wrap: anywhere;">Email us: <a href="mailto:w2cxm@cornell.edu">w2cxm@cornell.edu</a><br><br>401 Barton Hall<br>Ithaca, New York, U.S.<br><br>QSL via KD2EAT</div>
         <div style="background-color: white; width: 100%;"></div>
-        <div id="copyright" style="text-align: center; padding: 1rem 10%; font-size: 0.8em; line-height: 1.5em;">© 2026 by the Cornell Amateur Radio Club<br><br>This organization is a registered General Student Organization of Cornell University.<br><a href="https://hr.cornell.edu/about/workplace-rights/equal-education-and-employment" target="_blank">Equal Education and Employment</a></div>
+        <div id="copyright" style="text-align: center; padding: 1rem 10%; font-size: 0.8em; line-height: 1.5em;">© <span id="copyright-year"></span> by the Cornell Amateur Radio Club<br><br>This organization is a registered General Student Organization of Cornell University.<br><a href="https://hr.cornell.edu/about/workplace-rights/equal-education-and-employment" target="_blank" rel="noopener noreferrer">Equal Education and Employment</a></div>
         <div style="background-color: white; width: 100%;"></div>
         <div style="padding: 1rem 10%; max-width: 20vw; display: grid; grid-template-columns: 1fr 1fr; gap: 15%; place-items: center;">
-            <a target="_blank" href="https://www.facebook.com/W2CXM"><img style="width: 100%;" src="/data/social/facebook.png"></a>
-            <a target="_blank" href="https://www.instagram.com/cornellamateurradio_w2cxm"><img style="width: 100%;" src="/data/social/instagram.png"></a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/W2CXM"><img style="width: 100%;" src="/data/social/facebook.png" alt="Facebook"></a>
+            <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/cornellamateurradio_w2cxm"><img style="width: 100%;" src="/data/social/instagram.png" alt="Instagram"></a>
         </div>
         <div></div>
     </div>
@@ -18,3 +18,4 @@ var footer_data = `<div style="padding: 2vh;"></div>
 </footer>`;
 
 document.write(footer_data);
+document.getElementById('copyright-year').textContent = new Date().getFullYear();
